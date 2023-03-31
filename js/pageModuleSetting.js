b9081f130941ba62ab0cc74c578f470972ab959c
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-03-31 12:28:45 +0900

    JUN-1 페이지 모듈 js 수정

--- js/pageModuleSetting.js
+++ js/pageModuleSetting.js
@@ -1,4 +1,24 @@
-$(function() {
-    $('#header').load('../module/header.html');
-    $('#footer').load('../module/footer.html');
-});
+function loadModule(element) {
+    let targetElement = '#' + element;
+
+    let css = '../../css/module/' + element + '.css';
+    let html = '../../html/module/' + element + '.html';
+
+    $.get(css, function (data) {
+        $('head').append('<style class="' + element + '" >' + data + '</style>');
+        $(targetElement).load(html);
+    });
+}
+
+$(function () {
+    if (
+        document.title.includes("Login") ||
+        document.title.includes("Join") ||
+        document.title.includes("Find")
+    ) {
+        loadModule("logoNav")
+    } else {
+        loadModule('header')
+        loadModule('footer')
+    }
+})
