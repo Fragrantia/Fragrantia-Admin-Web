9ba5978401326efeb7485c53265c6495eb4f03d6
clldflwr <dlwjddlr97@gmail.com>
2023-04-04 15:10:23 +0900

    IK-8 고객센터 관리 페이지 필터링 미작동 현상 수정

--- js/buttonNav.js
+++ js/buttonNav.js
@@ -11,7 +11,18 @@ $(document).ready(function () {
     ) { 
         $('.button-nav').before('<div></div>');
         $('.button-nav').remove();
+        // select 요소 가져오기
+        const select = document.querySelector('#category');
         
+        // 새로운 option 요소 생성하기
+        const option1 = document.createElement('option');
+        const option2 = document.createElement('option');
+        option1.textContent = 'Store';
+        option2.textContent = 'Etc.';
+        
+        // select 요소에 새로운 option 요소 추가하기
+        select.appendChild(option1);
+        select.appendChild(option2);
     }
 
     document.addEventListener("click", function (event) {
@@ -35,6 +46,33 @@ $(document).ready(function () {
         }
     });
 
+    $('#category').change(function () {
+      var selectCategory = $(this).val();
+      var list = Array.from(document.getElementsByClassName('category'));
+
+      list.forEach(element => { //display 초기화
+          element.closest('tr').style.display = '';
+      });
+
+      if (selectCategory === 'All') { //전체 클릭시 리턴
+          return;
+      }
+
+      var filterList = list.filter(it => {
+          if ($(it).html() === 'Category') { //<th>Category</th>는 제외
+              return false;
+          }
+          return $(it).html() !== selectCategory;
+      });
+
+      filterList.forEach(element => {
+        if (element.textContent !== selectCategory) { // 선택된 카테고리가 아닌 경우에만 숨김 처리
+          element.closest('tr').style.display = 'none';
+        }
+      });
+      
+    });
+
     const modal = document.getElementById("modal");
     const modalButton = document.getElementsByClassName("plus-button")[0];
     const modalClose = document.getElementById("modal-close");
@@ -53,27 +91,5 @@ $(document).ready(function () {
         modal.style.display = "none";
     });
     
-    $('#category').change(function () {
-        var selectCategory = $(this).val();
-        var list = Array.from(document.getElementsByClassName('category'));
-
-        list.forEach(element => { //display 초기화
-            element.closest('tr').style.display = '';
-        });
-
-        if (selectCategory === '전체') { //전체 클릭시 리턴
-            return;
-        }
-
-        var filterList = list.filter(it => {
-            if ($(it).html() === 'Category') { //<th>Category</th>는 제외
-                return false;
-            }
-            return $(it).html() !== selectCategory;
-        });
-
-        filterList.forEach(element => {
-            element.closest('tr').style.display = 'none'; //선택안된 카테고리 숨김
-        });
-    });
+    
 });
