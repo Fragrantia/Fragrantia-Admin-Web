9e81a2162bf8fac3d5f5e5cb576d993b531fcc96
hj <gksgpwls0306@naver.com>
2023-04-04 17:12:00 +0900

    HJ-8 매장 토글 컨텐츠 수정

--- js/shop.js
+++ js/shop.js
@@ -1,6 +1,6 @@
 // colspan size 조절 함수
 let reColspan = function () {
-    if ($(window).width() <= 768) {
+    if ($(window).width() <= 425) {
         // colspan 속성이 4인 td 요소를 찾아서
         $("tr.shop-content td[colspan='5']").each(function () {
             // colspan 속성 값을 3으로 변경
@@ -23,7 +23,7 @@ $(document).ready(function () {
         content.fadeToggle('slow'); // 클릭한 요소 열기/닫기
         $('.shop-content').not(content).fadeOut(); // 이미 열려있는 요소 닫기
     });
-    $('input[type="checkbox"]').click(function(event) {
+    $('input[type="checkbox"]').click(function (event) {
         event.stopPropagation(); // 클릭 이벤트 전파 방지
     });
 
@@ -32,22 +32,41 @@ $(document).ready(function () {
 
     // chkAll을 클릭했을 때, chkAll이 체크되어 있으면, name이 chk인 input 태그의 checked 속성을 true로 설정
     // >> 전체 체크 박스를 체크하면 모든 체크 박스 체크 
-    $("#chkAll").click(function() {
-        if($("#chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
+    $("#chkAll").click(function () {
+        if ($("#chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
         else $("input[name=chk]").prop("checked", false);
-      });
-      // name이 chk인 input 태그를 클릭하면, name이 chk인 요소의 개수를 total 변수에 저장.
-      // name이 chk인 요소 중에 checked 되어 있는 요소의 개수를 checked 개수에 저장.
-      $("input[name=chk]").click(function() {
+    });
+    // name이 chk인 input 태그를 클릭하면, name이 chk인 요소의 개수를 total 변수에 저장.
+    // name이 chk인 요소 중에 checked 되어 있는 요소의 개수를 checked 개수에 저장.
+    $("input[name=chk]").click(function () {
         var total = $("input[name=chk]").length;
         var checked = $("input[name=chk]:checked").length;
-    
+
         // total 값이 checked의 값와 같지 않으면 id가 chkAll인 객체의 checked 속성을 false로 바꿈.
         // 그게 아니면 checked 속성을 true로 바꿈. 
-        if(total != checked) $("#chkAll").prop("checked", false);
-        else $("#chkAll").prop("checked", true); 
-      });
-  
+        if (total != checked) $("#chkAll").prop("checked", false);
+        else $("#chkAll").prop("checked", true);
+    });
+
+    //수정버튼 클릭시 on/off
+    $(".content_btn>button").click(function (event) {
+        event.preventDefault();
+        var changeInput = $(this).closest('div').prev().children('input');
+        var changeTextarea = $(this).closest('div').prev().children('textarea');
+
+        //수정사항 있을시 게시글 헤더에 반영
+        var originTitle = changeInput.val();
+
+        if (changeInput.attr('disabled') !== undefined ||
+            changeTextarea.attr('disabled') !== undefined) {
+            changeInput.removeAttr('disabled');
+            changeTextarea.removeAttr('disabled');
+        } else {
+            changeInput.prop('disabled', true);
+            changeTextarea.prop('disabled', true);
+        }
+    });
+
 });
 
 
